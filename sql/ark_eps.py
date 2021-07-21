7bae4f36775d96a8fa299de84de683a06fe511e4
lukerspace <lukerspace@gmail.com>
2021-07-22 02:33:39 +0800

    EPS sql syntax

--- sql/ark_eps.py
+++ sql/ark_eps.py
@@ -7,71 +7,81 @@ import pandas as pd
 from getpass import *
 import mysql.connector
 from mysql.connector import pooling
+import numpy as np
 pre_path = os.path.abspath("../backtrade/data/ark/")
 sys.path.append(pre_path)
 
+
 connection_pool = pooling.MySQLConnectionPool(
     host = "localhost",
-    user = getpass("SERVER_USER: "),
-    password = getpass("SERVER_PASSWORD:"),
-    
-    # password = "0000",
-    # user = "root",
-
+    # user = getpass("SERVER_USER: "),
+    # password = getpass("SERVER_PASSWORD:"),
+    password = "0000",
+    user = "root",
     database = "fund",
     charset = "utf8",
     auth_plugin='mysql_native_password' )
-connection_obj=connection_pool.get_connection()
+
 
 def sql_disconnect(connection_obj,table):
     if connection_obj.is_connected():
         table.close()
         connection_obj.close
 
-def ark_eps_insert(**kwarg):
-    if connection_obj.is_connected():
-        # sql_cmd=f"""INSERT INTO arkeps( date , est , act ,ticker) VALUES ('{a}',{b},{c},'{d}')"""
-        sql_cmd="select * from arkeps"
-        print("SQL CONNECTED")
-        table_cursor=connection_obj.cursor()
-        table_cursor.execute(sql_cmd)
-        print(table_cursor.fetchall())
-        # sql_disconnect(connection_obj,table_cursor)
-        # print("SQL sign out")
 
-    else:
-        print("SQL IS DISCONNECT")
+
+def syntax_str(**kwarg):
+    insert_values_str=''
+    for key in kwarg:
+        if type(kwarg[key]==str):
+            insert_values_str +=f"'{kwarg[key]}',"
+    return (insert_values_str)
+
+
+def syntax_int(**kwarg):
+    insert_values_int=''
+    for key in kwarg:
+            insert_values_int +=f"{kwarg[key]},"
+    return (insert_values_int[:-1])
+
+
+def ark_eps_insert():
+    ark=open('data/csv/ark.csv').readlines()
+    symbols=[holding.strip() for holding in ark][1:]
+    less_than_yaer=[]
+    for symbol in symbols:
+        try:
+            df=pd.read_csv(f'../backtrade/data/ark/{symbol}_EPS.txt'.format(symbol))
+            if len(df)<5:
+                df["ticker"]=symbol
+                less_than_yaer.append(symbol)
+                
+            else:
+                df["ticker"]=symbol
+                df=df.head()
+                df["Actual"]=df["Actual"].replace(to_replace='None',value="null")
+                df["Estimate"]=df["Estimate"].replace(to_replace='None',value="null")
+                for index,row in df.iterrows():
+                    tick=row["ticker"]
+                    dt=str(row["Date"])
+                    est=row["Estimate"]
+                    act=row["Actual"]
+
+                    val=syntax_str(ticker=tick,date=dt)+syntax_int(estimate=est,actual=act)
+                    sql_cmd=f"""INSERT INTO arkeps(ticker,date,est,act) VALUES ({val})"""
+
+                    connection_obj=connection_pool.get_connection()
+                    if connection_obj.is_connected():
+                        table_cursor=connection_obj.cursor()
+                        table_cursor.execute(sql_cmd)
+                        connection_obj.commit()
+                        connection_obj.close()
+                        print(sql_cmd)
+                
+        except:
+            print(symbol+" FIND NO EPS DATA")
+
+    return print("ARK EPS UPDATE")
 
 
 ark_eps_insert()
-
-ark=open('data/csv/ark.csv').readlines()
-symbols=[holding.strip() for holding in ark][1:]
-num=len(symbols)
-less_than_yaer=[]
-for symbol in symbols:
-    try:
-        df=pd.read_csv(f'../backtrade/data/ark/{symbol}_EPS.txt'.format(symbol))
-        if len(df)<5:
-            df["ticker"]=symbol
-            less_than_yaer.append(symbol)
-            
-        else:
-            df["ticker"]=symbol
-            df=df.head()
-            for index,row in df.iterrows():
-                # print(index,row["ticker"],row["Date"],row["Estimate"],row["Actual"])
-                print("---")
-        # ticker=df.loc[:,"ticker"]
-        # act=df.loc[:,"Actual"]
-        # md=df.loc[:,"Date"]
-        # est=df.loc[:,"Estimate"]
-        # print(ticker,act)
-        
-    except:
-        print("----")
-        print(symbol+" FIND NO EPS DATA")
-
-
-
-
