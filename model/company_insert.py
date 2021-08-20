071baa99465c5dce0694ea712c9ea7aac2f205af
lukerspace <lukerspace@gmail.com>
2021-08-20 23:47:57 +0800

    update

--- model/company_insert.py
+++ model/company_insert.py
@@ -20,7 +20,7 @@ for ticker in ark_dir:
             company_describe=company_data["description"][:3000]
             company_ind=company_data["industry"]
             company_web=company_data["website"]
-            # print(company_name,company_describe,company_ind,company_web)
+
             ark_company_insert=Company_Ark(company_ticker,company_name,company_describe,company_ind,company_web)
             db.session.add(ark_company_insert)
             db.session.commit()
@@ -70,17 +70,3 @@ for ticker in qqq_dir:
         print(ticker,"ERROR")
 
 
-# import mysql.connector
-# from mysql.connector import pooling
-# connection_pool =mysql.connector.pooling.MySQLConnectionPool(
-#     pool_name="mysql",
-#     pool_size=20,
-#     host="localhost",
-#     user="root",
-#     password="0000")
-# cur=connection_pool.get_connection()
-# my_cursor=cur.cursor()
-# my_cursor.execute("CREATE DATABASE company")
-# my_cursor.execute("SHOW DATABASESS")
-# for db in my_cursor:
-#     print(db)
--- model/rev_insert.py
+++ model/rev_insert.py
@@ -12,23 +12,23 @@ from apps import db
 from apps.module import *
 
 
-# ark_path="info/revenue/ark/"
-# ark_dir = os.listdir(ark_path)
-# for ticker in ark_dir:
-#    with open('info/revenue/ark/{}'.format(ticker)) as f:
-#         line=f.read()
-#         rev_json=json.loads(line)
-#         if len(rev_json)!=0:
+ark_path="info/revenue/ark/"
+ark_dir = os.listdir(ark_path)
+for ticker in ark_dir:
+   with open('info/revenue/ark/{}'.format(ticker)) as f:
+        line=f.read()
+        rev_json=json.loads(line)
+        if len(rev_json)!=0:
             
-#             symbol=ticker.split("_")[0]
-#             first=(rev_json["0"])
-#             second=(rev_json["1"])
-#             third=(rev_json["2"])
-#             ark_rev_insert=Rev_Ark(symbol,first,second,third)
-#             db.session.add(ark_rev_insert)
-#             db.session.commit()
-#         else:
-#             pass
+            symbol=ticker.split("_")[0]
+            first=(rev_json["0"])
+            second=(rev_json["1"])
+            third=(rev_json["2"])
+            ark_rev_insert=Rev_Ark(symbol,first,second,third)
+            db.session.add(ark_rev_insert)
+            db.session.commit()
+        else:
+            pass
 
 
 ark_path="info/revenue/spy/"
