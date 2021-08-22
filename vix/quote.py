947079aa074c28c7147282b2c09bba9c06beb307
lukerspace <lukerspace@gmail.com>
2021-08-22 17:20:22 +0800

    vix

--- vix/quote.py
+++ vix/quote.py
@@ -4,20 +4,35 @@ import yfinance as yf
 
 df_vix= yf.download("^VIX", start="2020-3-1",progress=False) 
 df_spy=yf.download("SPY", start="2020-3-1",progress=False) 
-
-# df_qqq=yf.download("^VIX", start="2020-1-1",progress=False)
+df_qqq=yf.download("QQQ", start="2020-3-1",progress=False)
+df_arkk=yf.download("ARKK", start="2020-3-1",progress=False)
 
 df_spy["VIX Open"]=df_vix["Open"]
 df_spy["VIX High"]=df_vix["High"]
 df_spy["VIX Low"]=df_vix["Low"]
 df_spy["VIX Close"]=df_vix["Close"]
 
-df_vix=df_vix.rename(columns={"Close":"vixclose","Open":"vixopen","Low":"vixlow","High":"vixhigh"})
+df_qqq["VIX Open"]=df_vix["Open"]
+df_qqq["VIX High"]=df_vix["High"]
+df_qqq["VIX Low"]=df_vix["Low"]
+df_qqq["VIX Close"]=df_vix["Close"]
 
+
+df_arkk["VIX Open"]=df_vix["Open"]
+df_arkk["VIX High"]=df_vix["High"]
+df_arkk["VIX Low"]=df_vix["Low"]
+df_arkk["VIX Close"]=df_vix["Close"]
+
+df_vix=df_vix.rename(columns={"Close":"vixclose","Open":"vixopen","Low":"vixlow","High":"vixhigh"})
 
 df_spy.reset_index(level=0, inplace=True)
 df_spy.to_csv("vix/spy2021.csv",index = False)
 
-
 df_vix.reset_index(level=0, inplace=True)
 df_vix.to_csv("vix/vix2021.csv",index = False)
+
+df_qqq.reset_index(level=0, inplace=True)
+df_qqq.to_csv("vix/qqq2021.csv",index = False)
+
+df_arkk.reset_index(level=0, inplace=True)
+df_arkk.to_csv("vix/arkk2021.csv",index = False)
--- vix/vix.py
+++ vix/vix.py
@@ -7,57 +7,192 @@ from config import *
 from VIXStrategy import VIXStrategy
 import yfinance as yf
 
-
-cerebro = bt.Cerebro()
-cerebro.broker.setcash(100000)
-
 from quote import *
 
-class SPYVIXData(bt.feeds.GenericCSVData):
-    lines = ('vixopen', 'vixhigh', 'vixlow', 'vixclose',)
 
-    params = (
-        ('dtformat', '%Y-%m-%d'),
-        ('date', 0),
-        ('spyopen', 1),
-        ('spyhigh', 2),
-        ('spylow', 3),
-        ('spyclose', 4),
-        ('spyadjclose', 5),
-        ('spyvolume', 6),
-        ('vixopen', 7),
-        ('vixhigh', 8),
-        ('vixlow', 9),
-        ('vixclose', 10)
-    )
-class VIXData(bt.feeds.GenericCSVData):
+def saveplots(cerebro, numfigs=1, iplot=True, start=None, end=None,
+             width=16, height=9, dpi=300, tight=True, use=None, file_path = '', **kwargs):
+
+        from backtrader import plot
+        if cerebro.p.oldsync:
+            plotter = plot.Plot_OldSync(volume=False,**kwargs)
+        else:
+            plotter = plot.Plot(volume=False,**kwargs)
+
+        figs = []
+        for stratlist in cerebro.runstrats:
+            for si, strat in enumerate(stratlist):
+                rfig = plotter.plot(strat, figid=si * 100,
+                                    numfigs=numfigs, iplot=iplot,
+                                    start=start, end=end, use=use)
+                figs.append(rfig)
+
+        for fig in figs:
+            for f in fig:
+                f.savefig(file_path, bbox_inches='tight')
+        return figs
+
+def vix_spy():
+    cerebro = bt.Cerebro()
+    cerebro.broker.setcash(100000)
+
+
+    class SPYVIXData(bt.feeds.GenericCSVData):
+        lines = ('vixopen', 'vixhigh', 'vixlow', 'vixclose',)
+
         params = (
- 
-        ('dtformat', '%Y-%m-%d'),
-        ('date', 0),
-        ('vixopen', 1),
-        ('vixhigh', 2),
-        ('vixlow', 3),
-        ('vixclose', 4),
-        ('volume', -1),
-        ('openinterest', -1)
-    )
+            ('dtformat', '%Y-%m-%d'),
+            ('date', 0),
+            ('spyopen', 1),
+            ('spyhigh', 2),
+            ('spylow', 3),
+            ('spyclose', 4),
+            ('spyadjclose', 5),
+            ('spyvolume', 6),
+            ('vixopen', 7),
+            ('vixhigh', 8),
+            ('vixlow', 9),
+            ('vixclose', 10)
+        )
+    class VIXData(bt.feeds.GenericCSVData):
+            params = (
+    
+            ('dtformat', '%Y-%m-%d'),
+            ('date', 0),
+            ('vixopen', 1),
+            ('vixhigh', 2),
+            ('vixlow', 3),
+            ('vixclose', 4),
+            ('volume', -1),
+            ('openinterest', -1)
+        )
 
-csv_file = os.path.dirname(os.path.realpath(__file__)) + "\\spy2021.csv"
-vix_csv_file = os.path.dirname(os.path.realpath(__file__)) + "\\vix2021.csv"
+    csv_file = os.path.dirname(os.path.realpath(__file__)) + "\\spy2021.csv"
+    vix_csv_file = os.path.dirname(os.path.realpath(__file__)) + "\\vix2021.csv"
 
-spyVixDataFeed = SPYVIXData(dataname=csv_file)
-vixDataFeed = VIXData(dataname=vix_csv_file)
+    spyVixDataFeed = SPYVIXData(dataname=csv_file)
+    vixDataFeed = VIXData(dataname=vix_csv_file)
 
-cerebro.adddata(spyVixDataFeed)
-cerebro.adddata(vixDataFeed)
+    cerebro.adddata(spyVixDataFeed)
+    cerebro.adddata(vixDataFeed)
 
-cerebro.addstrategy(VIXStrategy)
-cerebro.run()
+    cerebro.addstrategy(VIXStrategy)
+    cerebro.run()
+
+    saveplots(cerebro, file_path = 'vix/vix_spy.png') #run it
+    return print("=== update the latest VIX on DATE : {} ===".format(datetime.now()))
+
+def vix_qqq():
+    cerebro = bt.Cerebro()
+    cerebro.broker.setcash(100000)
+
+
+    class SPYVIXData(bt.feeds.GenericCSVData):
+        lines = ('vixopen', 'vixhigh', 'vixlow', 'vixclose',)
+
+        params = (
+            ('dtformat', '%Y-%m-%d'),
+            ('date', 0),
+            ('spyopen', 1),
+            ('spyhigh', 2),
+            ('spylow', 3),
+            ('spyclose', 4),
+            ('spyadjclose', 5),
+            ('spyvolume', 6),
+            ('vixopen', 7),
+            ('vixhigh', 8),
+            ('vixlow', 9),
+            ('vixclose', 10)
+        )
+    class VIXData(bt.feeds.GenericCSVData):
+            params = (
+    
+            ('dtformat', '%Y-%m-%d'),
+            ('date', 0),
+            ('vixopen', 1),
+            ('vixhigh', 2),
+            ('vixlow', 3),
+            ('vixclose', 4),
+            ('volume', -1),
+            ('openinterest', -1)
+        )
+
+    csv_file = os.path.dirname(os.path.realpath(__file__)) + "\\qqq2021.csv"
+    vix_csv_file = os.path.dirname(os.path.realpath(__file__)) + "\\vix2021.csv"
+
+    spyVixDataFeed = SPYVIXData(dataname=csv_file)
+    vixDataFeed = VIXData(dataname=vix_csv_file)
+
+    cerebro.adddata(spyVixDataFeed)
+    cerebro.adddata(vixDataFeed)
+
+    cerebro.addstrategy(VIXStrategy)
+    cerebro.run()
+
+    saveplots(cerebro, file_path = 'vix/vix_qqq.png') #run it
+    return print("=== update the latest VIX on DATE : {} ===".format(datetime.now()))
+
+
+def vix_arkk():
+    cerebro = bt.Cerebro()
+    cerebro.broker.setcash(100000)
+
+
+    class SPYVIXData(bt.feeds.GenericCSVData):
+        lines = ('vixopen', 'vixhigh', 'vixlow', 'vixclose',)
+
+        params = (
+            ('dtformat', '%Y-%m-%d'),
+            ('date', 0),
+            ('spyopen', 1),
+            ('spyhigh', 2),
+            ('spylow', 3),
+            ('spyclose', 4),
+            ('spyadjclose', 5),
+            ('spyvolume', 6),
+            ('vixopen', 7),
+            ('vixhigh', 8),
+            ('vixlow', 9),
+            ('vixclose', 10)
+        )
+    class VIXData(bt.feeds.GenericCSVData):
+            params = (
+    
+            ('dtformat', '%Y-%m-%d'),
+            ('date', 0),
+            ('vixopen', 1),
+            ('vixhigh', 2),
+            ('vixlow', 3),
+            ('vixclose', 4),
+            ('volume', -1),
+            ('openinterest', -1)
+        )
+
+    csv_file = os.path.dirname(os.path.realpath(__file__)) + "\\arkk2021.csv"
+    vix_csv_file = os.path.dirname(os.path.realpath(__file__)) + "\\vix2021.csv"
+
+    spyVixDataFeed = SPYVIXData(dataname=csv_file)
+    vixDataFeed = VIXData(dataname=vix_csv_file)
+
+    cerebro.adddata(spyVixDataFeed)
+    cerebro.adddata(vixDataFeed)
+
+    cerebro.addstrategy(VIXStrategy)
+    cerebro.run()
+
+    saveplots(cerebro, file_path = 'vix/vix_arkk.png') #run it
+    return print("=== update the latest VIX on DATE : {} ===".format(datetime.now()))
+
+
+vix_spy()
+vix_qqq()
+vix_arkk()
+
 # cerebro.plot(volume=False, savefig=True, figfilename='vix.png')
-
-figure = cerebro.plot(iplot=False,volume=False)[0][0].savefig('vix/vix.png')
+# figure = cerebro.plot(iplot=False,volume=False)[0][0].savefig('vix/vix.png')
 # figure.savefig('vix.png')
-print("update the latest VIX on DATE : {}".format(datetime.now()))
+
+
+
 
 
