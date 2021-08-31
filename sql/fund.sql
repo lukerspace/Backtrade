beab078af20d67681e5efaf995d325d616b03cc7
lukerspace <lukerspace@gmail.com>
2021-08-31 21:10:28 +0800

    update

--- sql/fund.sql
+++
@@ -1,9 +0,0 @@
-create database fund;
-
-use fund;
-
-create table arkeps(id int primary key auto_increment,
-	date date not null,
-    est float4 ,
-    act float4 ,
-    ticker varchar(24) not null);
